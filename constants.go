709db103fa176993fbfdb26f0423c0282972c398
suncnez <aleksandd0404@gmail.com>
2023-11-16 17:54:12 +0300

    first commit

---
+++ constants.go
@@ -0,0 +1,31 @@
+package main
+
+const (
+	EMOJI_COIN         = "\U0001FA99" // (coin)
+	EMOJI_SMILE        = "\U0001F642" // 🙂
+	EMOJI_SUNGLASSES   = "\U0001F60E" // 😎
+	EMOJI_WOW          = "\U0001F92A"
+	EMOJI_DONT_KNOW    = "\U0001F937" // 🤷
+	EMOJI_MONK         = "\U0001F9A7"
+	EMOJI_BICEPS       = "\U0001F4AA"   // 💪
+	EMOJI_BUTTON_START = "\U000025B6  " // ▶
+	EMOJI_BUTTON_END   = "  \U000025C0" // ◀
+
+	BUTTON_TEXT_PRINT_INTRO       = EMOJI_BUTTON_START + "Ok!" + EMOJI_BUTTON_END
+	BUTTON_TEXT_SKIP_INTRO        = EMOJI_BUTTON_START + "Skip." + EMOJI_BUTTON_END
+	BUTTON_TEXT_BALANCE           = EMOJI_BUTTON_START + "Current Balance" + EMOJI_BUTTON_END
+	BUTTON_TEXT_USEFUL_ACTIVITIES = EMOJI_BUTTON_START + "Useful Activities" + EMOJI_BUTTON_END
+	BUTTON_TEXT_REWARDS           = EMOJI_BUTTON_START + "Rewards" + EMOJI_BUTTON_END
+	BUTTON_TEXT_PRINT_MENU        = EMOJI_BUTTON_START + "MAIN MENU" + EMOJI_BUTTON_END
+
+	BUTTON_CODE_PRINT_INTRO       = "print_intro"
+	BUTTON_CODE_SKIP_INTRO        = "skip_intro"
+	BUTTON_CODE_BALANCE           = "show_balance"
+	BUTTON_CODE_USEFUL_ACTIVITIES = "show_useful_activities"
+	BUTTON_CODE_REWARDS           = "show_rewards"
+	BUTTON_CODE_PRINT_MENU        = "print_menu"
+
+	TOKEN_NAME_IN_OS             = "what-is-that"
+	UPDATE_CONFIG_TIMEOUT        = 60
+	MAX_USER_COINS        uint16 = 99
+)
---
+++ go.mod
@@ -0,0 +1,7 @@
+module telegramb
+
+go 1.21.1
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+
+
